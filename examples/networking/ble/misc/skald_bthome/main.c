a8250cad43cdba43d008a8620c61f7927ab45bf5
Martine S. Lenders <mail@martine-lenders.eu>
2024-11-16 11:46:27 +0100

    skald_bthome_encrypt: initial import

--- examples/networking/ble/misc/skald_bthome/main.c
+++ examples/networking/ble/misc/skald_bthome/main.c
@@ -37,6 +37,47 @@
 static skald_bthome_ctx_t _ctx;
 static skald_bthome_saul_t _saul_devs[CONFIG_BTHOME_SAUL_REG_DEVS];
 
+#ifdef ENCRYPTION_KEY
+int _get_encryption_key(void)
+{
+    static const char enc_str[] = ENCRYPTION_KEY;
+    static uint8_t encryption_key[SKALD_BTHOME_KEY_LEN] = { 0 };
+    size_t enc_str_len = strlen(enc_str);
+    uint8_t key_len = 0;
+
+    uint8_t shift = 4;
+    for (unsigned i = 0; i < enc_str_len; i++) {
+        char c = enc_str[i];
+        unsigned char offset;
+
+        if (c >= '0' && c <= '9') {
+            offset = '0';
+        }
+        else if (c >= 'a' && c <= 'f') {
+            offset = 'a' - 10;
+        }
+        else {
+            continue;
+        }
+        encryption_key[i / 2] |= (c - offset) << shift;
+        shift = (shift) ? 0 : 4;
+        key_len = (i / 2) + 1;
+        if (key_len > SKALD_BTHOME_KEY_LEN) {
+            printf("Key was too long: %u bytes\n", key_len);
+            return -ENOBUFS;
+        }
+    }
+
+    if (key_len < SKALD_BTHOME_KEY_LEN) {
+            printf("Key was too short: %u bytes\n", key_len);
+        return -EINVAL;
+    }
+    memcpy(_ctx.key, encryption_key, key_len);
+    _ctx.encrypt = 1;
+    return key_len;
+}
+#endif
+
 int main(void)
 {
     saul_reg_t *dev = saul_reg;
@@ -45,6 +86,21 @@ int main(void)
     ztimer_sleep(ZTIMER_MSEC, 2000);
     printf("Skald and the tale of Harald's home\n");
 
+#ifdef ENCRYPTION_KEY
+    int key_len;
+
+    key_len = _get_encryption_key();
+
+    if (key_len != SKALD_BTHOME_KEY_LEN) {
+        printf(
+            "Key should be of length 16 (32 chars), "
+            "was \"%s\" (%u chars)\n",
+            ENCRYPTION_KEY,
+            strlen(ENCRYPTION_KEY)
+        );
+        return 1;
+    }
+#endif
     _ctx.skald.update_pkt = NULL;
     _ctx.devs = NULL;
     _ctx.skald.pkt.len = 0;
